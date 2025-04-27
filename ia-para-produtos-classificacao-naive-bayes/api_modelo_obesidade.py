f7b83039b8ba6e686f090a97557b323cf1b9fe1e
Vitor Hugo da Costa Luz <vitorhugoclz@gmail.com>
2025-04-26 21:55:28 -0300

    Classificação Naive Bayes - Expor Modelo como API (usando Flask), Criar API para Modelo - Fazer Predição

--- ia-para-produtos-classificacao-naive-bayes/api_modelo_obesidade.py
+++ ia-para-produtos-classificacao-naive-bayes/api_modelo_obesidade.py
@@ -34,6 +34,33 @@ modelo_obesidade = joblib.load('modelo_obesidade.pkl')
 @app.route('/predict', methods=['POST'])
 @validate()
 def predict(body: RequestBody, **kwargs):
-    ...
+    # Transforma o body em um DataFrame
+    predict_df = pd.DataFrame(body.model_dump(), index=[1])
+
+    bins = [10, 20, 30, 40, 50, 60, 70]
+    bins_ordinal = [0, 1, 2, 3, 4, 5]
+    predict_df['Faixa_Etaria'] = pd.cut(predict_df['Idade'], bins=bins, labels=bins_ordinal, include_lowest=True)
+
+    # Deixa as k melhores features
+    predict_df = predict_df[[
+        "Historico_Familiar_Sobrepeso",
+        "Consumo_Alta_Caloria_Com_Frequencia",
+        "Consumo_Alimentos_entre_Refeicoes",
+        "Monitora_Calorias_Ingeridas",
+        "Nivel_Atividade_Fisica",
+        "Nivel_Uso_Tela",
+        "Transporte_Caminhada",
+        "Faixa_Etaria"
+    ]]
+
+    # Faz a predição
+    y_pred = modelo_obesidade.predict(predict_df)[0].astype(int)
+
+    # Retornar valor na API
+    return jsonify({
+        'obesidade': y_pred.tolist(),
+    })
 
 
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
